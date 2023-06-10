8b4dcc4f7edc05ba7eb147aab044241d22cbcf65
shytuleva24 <shutyleva@gmail.com>
2023-06-10 21:46:51 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ThreeSceneComponent } from './three-scene/three-scene.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TechnologyComponent } from './technology/technology.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ThreeSceneComponent,
+    AboutComponent,
+    PortfolioComponent,
+    TechnologyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
